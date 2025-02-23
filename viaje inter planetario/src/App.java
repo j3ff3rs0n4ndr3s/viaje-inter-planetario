8549356af2e5b807a3a2b165a28f69ef294def68
devizanagizero <carlosmanuelmisena@gmail.com>
2025-02-22 21:59:39 -0500

    version 2

---
+++ viaje inter planetario/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, soldado!");
+        System.out.println("hola json");
+        System.out.println("version 2");
+        System.out.println("holaaa");
+    }
+}
